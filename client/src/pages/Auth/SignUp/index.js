dff61c87847fd1de4832f5a042bdb69e9d5fff8b
berksengul17 <berksengul17@gmail.com>
2023-05-22 23:41:21 +0300

    create sign up page

--- client/src/pages/Auth/SignUp/index.js
+++ client/src/pages/Auth/SignUp/index.js
@@ -1,5 +1,58 @@
+import React from "react";
+import {
+	Flex,
+	Box,
+	Heading,
+	FormControl,
+	FormLabel,
+	Input,
+	Button,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import validationSchema from "./validations";
+
 function SignUp() {
-	return <div>Sign up</div>;
+	const formik = useFormik({
+		initialValues: {
+			email: "",
+			password: "",
+			passwordConfirm: "",
+		},
+		onSubmit: (values, bag) => {
+			console.log(values);
+		},
+		validationSchema,
+	});
+	return (
+		<div>
+			<Flex align={"center"} width={"full"} justifyContent={"center"}>
+				<Box pt={10}>
+					<Box textAlign={"center"}>
+						<Heading>Sign Up</Heading>
+					</Box>
+					<Box my={5} textAlign={"left"}>
+						<form action='' onSubmit={() => {}}>
+							<FormControl>
+								<FormLabel>E-mail</FormLabel>
+								<Input name='email' />
+							</FormControl>
+							<FormControl mt={4}>
+								<FormLabel>Password</FormLabel>
+								<Input name='password' type={"password"} />
+							</FormControl>
+							<FormControl mt={4}>
+								<FormLabel>Password Confirm</FormLabel>
+								<Input name='password' type={"password"} />
+							</FormControl>
+							<Button mt={4} width={"full"} type={"submit"}>
+								Sign Up
+							</Button>
+						</form>
+					</Box>
+				</Box>
+			</Flex>
+		</div>
+	);
 }
 
 export default SignUp;
